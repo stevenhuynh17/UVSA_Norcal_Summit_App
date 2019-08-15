cab21107df8c935ada6966c6d69a611576e91349
Steven <stevenhuynh17@gmail.com>
2019-08-14 20:06:16 -0700

    user can now sign out

--- screens/FamilyScreen.js
+++ screens/FamilyScreen.js
@@ -1,28 +1,52 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import {
+  Image,
   Text,
   View,
-  ScrollView
+  ScrollView,
+  AsyncStorage
 } from 'react-native';
 import {
-  ListItem,
-  Image
+  Card, ListItem, Button, Icon, SocialIcon
 } from 'react-native-elements';
 
 import { MonoText } from '../components/StyledText';
 
 class FamilyScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: 'Family',
+      headerRight: (
+        <Button
+          title="Sign out"
+          buttonStyle={{
+            marginRight: 5
+          }}
+          onPress={navigation.getParam('signOut')}
+        />
+      )
+    }
+  }
+
+  componentDidMount() {
+    this.props.navigation.setParams({ signOut: this._signOutAsync});
+  }
+
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate('Auth');
+  }
 
 
   render() {
+    const famHead= {
+      name: 'Amy Farha',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President'
+    }
     const list = [
       {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President'
-      },
-      {
         name: 'Chris Jackson',
         avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
         subtitle: 'Vice Chairman'
@@ -58,26 +82,74 @@ class FamilyScreen extends React.Component {
       {
         name: 'brynn',
         avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+      },
+      {
+        name: 'Amy Farha',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        subtitle: 'Vice President'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'brynn',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+      },
+      {
+        name: 'Amy Farha',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        subtitle: 'Vice President'
+      },
+      {
+        name: 'Chris Jackson',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman'
+      },
+      {
+        name: 'brynn',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+      },
+      {
+        name: 'Amy Farha',
+        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        subtitle: 'Vice President'
       }
     ]
+    const avatar_size = 150
 
     return(
       <View>
+        <ListItem
+          title={famHead.name}
+          subtitle={famHead.subtitle}
+          leftAvatar={{
+            source: {uri: famHead.avatar_url},
+            rounded: true,
+            size: 'large'
+          }}
+        />
         <ScrollView>
           {
-            list.map((person, index) => (
-              <Image
-                key={index}
-                source={{uri: person.avatar_url}}
-                style={{
-                  width: 200,
-                  height: 200,
-                  borderRadius: 200/2
-                }}
-              />
-            ))
+            list.map((users, index) => {
+              return (
+                <ListItem
+                  key={index}
+                  title={users.name}
+                  subtitle={users.subtitle}
+                  leftAvatar={{
+                    source: {uri: users.avatar_url},
+                    rounded: true,
+                    size: 'large'
+                  }}
+                  bottomDivider={true}
+                />
+              )
+            })
           }
         </ScrollView>
+
       </View>
     )
   }
