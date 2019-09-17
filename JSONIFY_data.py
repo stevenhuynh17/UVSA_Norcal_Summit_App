63a8e9afe2a15c8dc36ee8b27fe32b990b94c27e
Steven <stevenhuynh17@gmail.com>
2019-09-16 18:53:10 -0700

    able to extract specific questions from data

--- JSONIFY_data.py
+++ JSONIFY_data.py
@@ -1,1 +1,48 @@
 import json
+import csv
+
+with open('data/attendees/captainCrunch.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    family_Header = ["Name",
+    "Phone Number", "Preferred Email",
+    "Are you a Staff or Attendee?", "School Affiliation", "T-shirt Size",
+    "Preferred Gender Pronouns", "Would you prefer a co-ed room?",
+    "Please upload a professional headshot",
+    "What's your social media? Facebook",
+    "Emergency Contact Full Name",
+    "Emergency Contact Phone Number",
+    "Emergency Contact Relationship to You"]
+
+    data = {
+        "attendees": {}
+    }
+    # data["name"] = "STEVEN"
+    print(data)
+
+    def extractHeaders(data):
+        headers = list(data.keys())
+        print(enumerate(headers))
+        question_Num = [2, 3, 5, 6, 7, 10, 12, 16, 18, 26, 27, 28]
+        question_Str = []
+
+        for idx, header in enumerate(headers):
+            # Uncomment whenever you want to see numbering of questions
+            # print(idx, header)
+            for i in question_Num:
+                if(i == idx):
+                    question_Str.append(header)
+        return question_Str
+
+    for row in csv_reader:
+        if line_count == 0:
+            focus_questions = extractHeaders(row)
+            for header in focus_questions:
+                print(row[header])
+            line_count += 1
+
+
+
+
+    with open("data_file.json", "w") as write_file:
+        write_file.write(json.dumps(data, indent=4))
